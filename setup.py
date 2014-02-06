efd76f4d7c9008851ce10ef68439a63ad8bef375
Derek Schultz <derek@derekschultz.net>
2014-02-06 16:44:55 -0500

    Adds setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(name="django-shop-braintree",
+      version="0.1",
+      description="A Braintree payment backend for django-shop",
+      author="Derek Schultz",
+      author_email='derek@derekschultz.net',
+      platforms=["any"],
+      license="MIT",
+      url="http://github.com/derek-schultz/django-shop-braintree",
+      packages=find_packages(),
+      install_requires=["braintree>=2.27.0"],
+      )
